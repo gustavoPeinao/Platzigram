bc0991526b9ec5c3ab4fc203364e564a99667129
GustavoMamani <gustavopeinao@gmail.com>
2018-10-08 23:30:05 -0300

    Error Import  AppCompatActivity

--- app/src/main/java/com/platzi/platzigram/ReferenciaActivity.java
+++ app/src/main/java/com/platzi/platzigram/ReferenciaActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toolbar;
 import android.view.View;
 
 public class ReferenciaActivity extends AppCompatActivity {
